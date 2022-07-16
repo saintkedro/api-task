09aa248dadb44d16b4b7154e9fab0618da6974f4
saintkedro <isongkingsley101@gmail.com>
2022-07-16 10:52:01 +0100

    update urls.py

--- I4G001464ILO/I4G001464ILO/urls.py
+++ I4G001464ILO/I4G001464ILO/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/links/", include("links.urls"))
+    path("api/links/", include("links.urls")),
+    path('', admin.site.urls),
 ]
